47752d2e8e0ede88bead02a390504089965ef651
AmpiroMax <patrat2015@yandex.ru>
2022-10-23 18:42:18 +0300

    Strange function was added

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,12 @@
 """ Made by Ampiro"""
 
+
+def text_generator():
+    """ Text generator """
+    text = "Lorem ipsum dolor sit amet"
+    for word in text.split():
+        yield word
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    print(list(text_generator()))
