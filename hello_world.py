d0a6950f306df501d2a5d51f460250358cd944c9
AmpiroMax <patrat2015@yandex.ru>
2022-10-23 18:55:41 +0300

    So useless commit im crying

--- hello_world.py
+++ hello_world.py
@@ -8,5 +8,14 @@ def text_generator():
         yield word
 
 
+def fast_forward_function():
+    """
+    This function has description only because
+    pylint made me do this
+    """
+    text = "Are there any reasons to live except to gain knowledge?"
+    return text
+
+
 if __name__ == "__main__":
     print(list(text_generator()))
